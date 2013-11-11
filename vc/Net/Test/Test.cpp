d6b3d1b656c3a381f128f1055ab27610f918d311
GuiRongLiu <16839242@qq.com>
2013-11-11 13:26:34 +0800

    new
    add public

--- vc/Net/Test/Test.cpp
+++ vc/Net/Test/Test.cpp
@@ -3,40 +3,13 @@
 #include <Windows.h>
 #pragma comment (lib, "Winmm.lib") 
 
-float InvSqrt (float x) 
-{
-    float xhalf = 0.5f*x;
-    int i = *(int*)&x;
-    i = 0x5f3759df - (i>>1);
-    x = *(float*)&i;
-    x = x*(1.5f - xhalf*x*x);
-    return x;
-}
-
-void test() {
-	float value = 0.0f;
-	float arg = 101000003.0f;
-	DWORD sys_time = timeGetTime();
-	int looper = 0;
-	for (;looper < 10000000; looper++) {
-		value = InvSqrt(arg);
-	}
-	printf("InvSqrt() used time: %d , value: %f \n", timeGetTime() - sys_time, value);
-
-	sys_time = timeGetTime();
-	looper = 0;
-	for (;looper < 10000000; looper++) {
-		value = sqrt(arg);
-	}
-	printf("sqrt() used time: %d , value: %f \n", timeGetTime() - sys_time, value);
-}
-
 int _tmain(int argc, _TCHAR* argv[])
 {
-	//test();
-		
-	
+	volatile bool b = true;
 
+	for (int x = 1; x != 1; x ++) {
+		int xxx = 1;
+	}
 
 	return 0;
 }
